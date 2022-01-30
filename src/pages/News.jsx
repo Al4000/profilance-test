dbad0129f46c0d2867dd0252922795e5f5dc4763
Alexander <kazakov_ai87@mail.ru>
2022-01-30 23:17:44 +0300

    'fix'

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -53,7 +53,7 @@ function News({ isAdmin, isAuth, news }) {
 								: card.confirmed && <NewsItem card={ card } isAdmin={ isAdmin } key={ index } id={ index } />
 							))
 						}
-						{ searchInput.length && !searchNews.length &&
+						{ !!searchInput.length && !searchNews.length &&
 							<h3>
 								Не найдено ни одной новости :(
 							</h3>
